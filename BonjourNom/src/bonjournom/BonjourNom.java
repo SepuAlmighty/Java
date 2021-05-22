09a39b3978a246d8f9acef961fecb5e1b3de175a
SepuAlmighty <nader.berjaoui@gmail.com>
2021-05-22 09:14:48 -0400

    Exemple test

---
+++ BonjourNom/src/bonjournom/BonjourNom.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bonjournom;
+import aleatoire.*;
+
+/**
+ *
+ * @author nader
+ */
+public class BonjourNom {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Person john = new Person("John","Doe", 35);
+        System.out.println("Prenom : " + john.getFirstName());
+        System.out.println("Nom : " + john.getLastName());
+        System.out.println("Age : " + john.getAge());
+        
+        Person nader = new Person(args[0],args[1],Integer.parseInt(args[2]));
+        System.out.println("Bonjour " + nader.getFirstName() +" "+ nader.getLastName() + " tu a " + nader.getAge());
+        
+        System.out.println("Bonjour John Doe");
+    }
+    
+}
